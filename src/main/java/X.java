4449a12b46d2b79dc809ed0a4eb4e377326751b3
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-02-04 21:31:16 +0100

    class + tests

---
+++ src/main/java/X.java
@@ -0,0 +1,12 @@
+/**
+ * Created by mtumilowicz on 2019-02-04.
+ */
+class X {
+    final String name;
+    final int count;
+
+    X(String name, int count) {
+        this.name = name;
+        this.count = count;
+    }
+}
---
+++ src/test/java/ObjectCreationTest.java
@@ -0,0 +1,28 @@
+import org.junit.Test;
+
+import java.lang.reflect.InvocationTargetException;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * Created by mtumilowicz on 2019-02-04.
+ */
+public class ObjectCreationTest {
+    @Test
+    public void create() throws NoSuchMethodException,
+            IllegalAccessException,
+            InvocationTargetException,
+            InstantiationException {
+        X x = X.class.getDeclaredConstructor(String.class, int.class).newInstance("a", 1);
+        assertThat(x.name, is("a"));
+        assertThat(x.count, is(1));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void create_wrongArguments() throws NoSuchMethodException,
+            IllegalAccessException,
+            InvocationTargetException,
+            InstantiationException {
+        X.class.getDeclaredConstructor(String.class, int.class).newInstance("a");
+    }
+}
